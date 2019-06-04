7fc87323017ab6bca6671aad5fd5daeb63a1ad57
Antonio Rendina <antonio.rendina@bluemetrix.com>
2019-06-04 10:40:50 +0200

    downloaded software and install documentation

---
+++ archive/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -c http://www.public-software-group.org/mercurial/liquid_feedback_frontend/raw-file/tip/INSTALL.html
+wget -c http://www.public-software-group.org/pub/projects/liquid_feedback/backend/v3.2.2/liquid_feedback_core-v3.2.2.tar.gz
+wget -c https://www.public-software-group.org/pub/projects/liquid_feedback/frontend/v3.2.1/liquid_feedback_frontend-v3.2.1.tar.gz
